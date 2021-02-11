d957faa81cc3fb8712f5f77359a1a0ad4dc949cf
Jasper <jasperhavenhand@gmail.com>
2021-02-11 11:29:28 +0000

    Creating a TemporalGraphHandler class for performing the token dissemmination in each timestep.

--- src/Testing.java
+++ src/Testing.java
@@ -1,35 +1,42 @@
+import org.apache.flink.api.common.operators.Order;
 import org.gradoop.temporal.io.impl.csv.TemporalCSVDataSource;
 import org.gradoop.temporal.model.impl.TemporalGraph;
+import org.gradoop.temporal.model.impl.TemporalGraphCollection;
+import org.gradoop.temporal.model.impl.functions.predicates.AsOf;
 import org.gradoop.temporal.model.impl.pojo.TemporalEdge;
-import org.gradoop.temporal.model.impl.pojo.TemporalGraphHead;
 import org.gradoop.temporal.model.impl.pojo.TemporalVertex;
 
 import data.temporal.TemporalDataFactory;
-import data.temporal.TemporalDataFactory.inputType;
 import utilities.Log;
 
 public class Testing {
 	
 	public static void main (String[] args) {
 		try {
-			TemporalCSVDataSource data = TemporalDataFactory.createCSVDataSource(
-				"C:\\Users\\Student\\Documents\\Fourth Year\\COMP390 - Honours Year Project\\NSense_Traces_Set2_CRAWDAD",
-				inputType.NSENSE,"NSense_test");
+//			TemporalCSVDataSource data = TemporalDataFactory.createCSVDataSource(
+//				"C:\\Users\\Student\\Documents\\Fourth Year\\COMP390 - Honours Year Project\\NSense_Traces_Set2_CRAWDAD",
+//				inputType.NSENSE,"NSense_test");
 			
-//			TemporalCSVDataSource data = TemporalDataFactory
-//					.loadCSVDataSource("C:\\Users\\Student\\eclipse-workspace\\Honours-Year-Project\\data\\NSense_test");
-
+			TemporalCSVDataSource data = TemporalDataFactory
+					.loadCSVDataSource("C:\\Users\\Student\\eclipse-workspace\\Honours-Year-Project\\data\\NSense_test");
+			
 			TemporalGraph graph = data.getTemporalGraph();
+			System.out.println(graph.getEdges().count());
 			
-			for (TemporalGraphHead head: graph.getGraphHead().collect()) {
-				System.out.println(head.getPropertyValue("name"));
-			}
-			for (TemporalVertex vertex: graph.getVertices().collect()) {
-				System.out.println(vertex.getPropertyValue("name"));
-			}
-			for (TemporalEdge edge: graph.getEdges().collect()) {
-				System.out.println(edge.getId());
-			}
+			TemporalEdge edge = graph.getEdges().sortPartition("validTime", Order.ASCENDING).collect().get(0);
+			long timestamp = edge.getValidFrom();
+			System.out.println(timestamp);
+			
+			System.out.println(graph.snapshot(new AsOf(timestamp)).getEdges().count());
+			
+//			String query = "MATCH (v1)-[]->(v2) WHERE v1.infected = false AND v2.infected = true";
+//			
+//			TemporalGraphCollection newInfections = graph.query(query);
+////			newInfections.print();
+//			for (TemporalVertex vertex: newInfections.getVertices().collect()) {
+//				System.out.println(vertex.getPropertyValue("name"));
+//			}
+			
 		}catch (Exception e) {
 			Log.getLog("data_sources_log").writeException(e);
 			e.printStackTrace();
---
+++ src/graph/temporal/TemporalGraphHandler.java
@@ -0,0 +1,47 @@
+package graph.temporal;
+
+import java.time.LocalDateTime;
+
+import org.apache.flink.api.common.operators.Order;
+import org.gradoop.temporal.model.impl.TemporalGraph;
+import org.gradoop.temporal.model.impl.functions.predicates.AsOf;
+
+import utilities.Log;
+
+public final class TemporalGraphHandler {
+
+	private TemporalGraph completeGraph;
+	private TemporalGraph currentGraph;
+	private int transmissionRisk;
+	private Long currentTimestamp;
+	/** The name of the log file that will be used by this class. */
+	private static String LOG_NAME = "graphs_log";
+	
+	public TemporalGraphHandler(TemporalGraph graph, int transmissionRisk) {
+		try {
+			completeGraph = graph;
+			this.transmissionRisk = transmissionRisk;
+			
+			currentTimestamp = completeGraph.getEdges()
+					.sortPartition("transactionTime", Order.ASCENDING)
+					.collect().get(0).getTxFrom();
+			
+			currentGraph = completeGraph.snapshot(new AsOf(currentTimestamp));
+		} catch (Exception e) {
+			Log.getLog(LOG_NAME).writeException(e);
+			e.printStackTrace();
+		}
+	}
+	
+	public TemporalGraph getCompleteGraph() {
+		return completeGraph;
+	}
+	
+	public TemporalGraph getCurrentGraph() {
+		return currentGraph;
+	}
+	
+	public void nextTimeStep() {
+		
+	}
+}
